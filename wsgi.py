e162c5b661e6baa004444505a06e4869a3abc526
hemanthhari6281 <124413439+hemanthhari6281@users.noreply.github.com>
2024-08-11 21:11:35 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for security_of_cyber_physical_systems.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'security_of_cyber_physical_systems.settings')
+application = get_wsgi_application()
